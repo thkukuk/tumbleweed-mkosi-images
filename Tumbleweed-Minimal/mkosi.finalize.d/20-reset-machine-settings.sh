1bdbb5b7e09a62fd3b22cefc89ffd1d732543a57
Thorsten Kukuk <kukuk@suse.com>
2026-01-19 11:59:03 +0100

    20-reset-machine-settings.sh: cleanup /var/cache, too

--- Tumbleweed-Minimal/mkosi.finalize.d/20-reset-machine-settings.sh
+++ Tumbleweed-Minimal/mkosi.finalize.d/20-reset-machine-settings.sh
@@ -1,6 +1,8 @@
 #! /usr/bin/bash
 
-echo "*** reset machine settings ***"
+echo "*** reset machine settings and cache ***"
 #rm -fv /buildroot/etc/machine-id
 rm -fv /buildroot/var/lib/zypp/AnonymousUniqueId \
        /buildroot/var/lib/systemd/random-seed
+rm -fv /buildroot/init
+rm -rfv /buildroot/var/cache/{gio-2.0,man,zypp}
